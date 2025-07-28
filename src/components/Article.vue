6cb56d5bc12ec4ce72b0a0c5efad5210f4a80f8b
AlanNapieralski <a.napieralski@lancaster.ac.uk>
2025-07-28 15:11:15 +0100

    implemented read time count

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -27,6 +27,32 @@ const article = computed(() => {
     )
 })
 
+console.log(article.value?.attributes.body.value)
+
+function calculateReadingTime(text: string): number {
+    const noImages = text.replace(/<img[^>]*>/g, '')
+    const noHrefs = noImages.replace(/<a[^>]*>(.*?)<\/a>/g, '$1')
+    const plainText = noHrefs
+        .replace(/<\/(p|h[1-6]|div|blockquote)>/g, ' ') // Add space after closing tags
+        .replace(/<[^>]*>/g, '')
+        .replace(/&nbsp;/g, ' ')
+        .replace(/&[a-z]+;/g, ' ')
+    
+    const words = plainText
+        .trim()
+        .split(/\s+/)
+        .filter(word => word.length > 0)
+    
+    console.log(words)
+    
+    const minutes = Math.ceil(words.length / 238)
+    return Math.max(1, minutes)
+}
+
+const readingTime = computed(() => 
+    calculateReadingTime(article.value?.attributes.body.value || '')
+)
+
 </script>
 
 <template>
@@ -40,7 +66,11 @@ const article = computed(() => {
         </div>
     </div>
     <div v-else-if="article" class="flex flex-col space-y-6">
-        <Title :title="article.attributes.title" :date="article.attributes.created" :timeToRead="0" />
+        <Title 
+            :title="article.attributes.title" 
+            :date="article.attributes.created" 
+            :timeToRead="readingTime" 
+        />
         <Body :body="article.attributes.body.value"/>
     </div>
     <div v-else>
